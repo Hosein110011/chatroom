7a44269f3276e849bf4f060883198ddc0ce63e1e
hosein <immortalhosein110011@gmail.com>
2023-08-05 01:34:08 +0330

    create room model

--- base/views.py
+++ base/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+rooms = [
+    {'id': 1, 'name':'Learn Python'},
+     {'id': 2, 'name':'Learn Java'},
+      {'id': 3, 'name':'Learn Android'}
+]
+
+
+
+def home(request):
+    return render(request, 'base/home.html', {'rooms':rooms})
+
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}        
+    
+    return render(request, 'base/room.html', context)
+
+
+
+
+
+
+
+
+
+
+
