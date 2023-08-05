29d925858bda12971bca155d7b049af3924f7cbf
hosein <immortalhosein110011@gmail.com>
2023-08-05 23:52:55 +0330

    commit all.

---
+++ base/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+
+
+
+urlpatterns = [
+    path('',views.getRoutes),
+    path('rooms/', views.getRooms),
+    path('rooms/<str:pk>/', views.getRooms)
+]
+
+
+
+
+
+
