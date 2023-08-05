f224feb17465110f7e652c9cc9d091e9e5bbc375
hosein <immortalhosein110011@gmail.com>
2023-08-05 04:34:46 +0330

    fix problem of update view funtion

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room
-
+from .forms import RoomForm
 
 # rooms = [
 #     {'id': 1, 'name':'Learn Python'},
@@ -23,11 +23,34 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
 
 
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if form.is_valid():
+        form.save(commit=True)
+        return redirect('home')
+    
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
 
 
-
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':room})
 
 
 
